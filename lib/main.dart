36d623feecdaeaffb1af914539e1feee23f015ba
Siddhant Vij <siddhantvij201@gmail.com>
2024-01-30 20:14:10 +0530

    Firebase Initialization
    - Installed the Firebase CLI.
    - Installed the FlutterFire CLI.
    - Configured this app to use Firebase.
    - Initialized Firebase in this app.
    - Flutter Firebase Initialization Test

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'screens/chat_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/registration_screen.dart';
 import 'screens/welcome_screen.dart';
 
-void main() => runApp(const FlashChat());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Firebase Project (Follow Docs)
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   const FlashChat({super.key});
